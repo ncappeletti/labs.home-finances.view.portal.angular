e464e3d7b933d658faa1c8fc79f7a73564199b6c
ncappeletti <cappe335@gmail.com>
2026-07-22 14:54:29 -0300

    initial

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home-page.component';
+
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: '**', redirectTo: '' }
+];
